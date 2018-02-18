d1ceba8cf975d912e3b795f88680b4050b519fb3
kaysarov <kaysarov@example.com>
2018-02-18 20:26:48 +0300

    Added views to create/edit blog post inside the site.

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,7 +1,14 @@
 from django.urls import include, path
 from . import views
-
+'''
+urlpatterns = [
+    path('', views.simple, name='simple'),
+    path(r'^post/(?P<pk>\d+)/', views.post_detail, name='post_detail'),
+]
+'''
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('simple/', views.simple, name='simple'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<pk>/', views.post_detail, name='post_detail'),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-import django
 import numpy as np
-import matplotlib.pyplot as plt
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostForm
 
 
 def post_list(request):
@@ -14,10 +12,38 @@ def post_list(request):
 
 def simple(request):
     x = np.random.random(1000)
-    num_bins = 10
-    fig, ax = plt.subplots()
-    ax.hist(x, num_bins)
-    canvas = FigureCanvas(fig)
-    response = django.http.HttpResponse(content_type='image/png')
-    canvas.print_png(response)
-    return response
+    return render(request, 'firstapp/simple.html', {'x': x})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'firstapp/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'firstapp/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'firstapp/post_edit.html', {'form': form})
