4e1475fcabc741fd91b3039dd5a1ce45024169ae
kaysarov <kaysarov@example.com>
2018-02-18 20:31:27 +0300

    t

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import include, path
 from . import views
-'''
+
 urlpatterns = [
     path('', views.simple, name='simple'),
-    path(r'^post/(?P<pk>\d+)/', views.post_detail, name='post_detail'),
 ]
+
 '''
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -12,3 +12,4 @@ urlpatterns = [
     path('post/<pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<pk>/', views.post_detail, name='post_detail'),
 ]
+'''
